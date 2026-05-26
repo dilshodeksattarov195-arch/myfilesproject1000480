e1d173c79fb070a6dfe243e587c87b8c205dd1eb
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-26 14:54:32 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class productEalculateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = productEalculateWorker()
+    print("Worker engine initialized.")
